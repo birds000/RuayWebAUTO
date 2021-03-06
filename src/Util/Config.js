4272a211ad7ce8ab8f4f9c5af96affaab2c28ad3
Kittikorn <61011270001@msu.ac.th>
2021-03-06 18:59:31 +0700

    update config

--- src/Util/Config.js
+++ src/Util/Config.js
@@ -1,3 +1,4 @@
-export const URL = "http://localhost:8080"
+// export const URL = "http://localhost:8080"
+export const URL = "http://178.128.221.76:8080"
 
 // export const URL = "http://192.168.1.46:8080/api/v1/";
